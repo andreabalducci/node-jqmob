3e3b0d00cee59f88a4fa73315b798ef9de3f821d
Andrea <andrea@mbp-andrea.local>
2012-02-02 22:27:27 +0100

    aggiunto jquerymobile

--- app.js
+++ app.js
@@ -50,5 +50,9 @@ app.get('/album/:id', function(req, res){
     });
 });
 
+app.get('/jqm', function(req, res){
+    res.redirect('/jquerymobile/demos/index.html');
+});
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
