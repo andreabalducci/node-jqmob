1f04d41d088df1b4ad166569c60174961033f7e4
Andrea Balducci <mtb.snowboard@gmail.com>
2012-02-22 09:04:42 +0100

    aggiunto supporto CORS

--- app.js
+++ app.js
@@ -14,12 +14,20 @@ var app = module.exports = express.createServer();
 app.enable('jsonp callback');
 
 // Configuration
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(allowCrossDomain);
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -55,6 +63,13 @@ app.get('/api/album/:id', function(req, res){
 
 app.get('/jqm', function(req, res){
     res.redirect('/jquerymobile/demos/index.html');
+});
+
+app.post('/api/album/', function(req,res){
+    console.log('start');
+    console.log(req.body);
+    console.log('stop');
+    res.json({status: req.body});
 });
 
 app.listen(3000);
