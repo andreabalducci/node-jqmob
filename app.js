9e64805ba02ebdb4694396512a00b642c03a7720
Andrea Balducci <mtb.snowboard@gmail.com>
2012-02-22 18:19:52 +0100

    impostato il timeout di connessione da parametro querystring aggiunto il salvataggio dell'evento

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes');
+    , routes = require('./routes');
 
 var EventiProvider = require('./EventiProvider.js').EventiProvider;
 
@@ -14,7 +13,7 @@ var app = module.exports = express.createServer();
 app.enable('jsonp callback');
 
 //CORS middleware
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -22,54 +21,55 @@ var allowCrossDomain = function(req, res, next) {
 };
 
 // Configuration
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(allowCrossDomain);
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(allowCrossDomain);
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
 // Routes
 
 app.get('/', routes.index);
 
-app.get('/api/eventi', function(req, res){
-    provider.findAll(function(err, data){
-        if(err) throw err;
-
-        setTimeout(function(){
+app.get('/api/eventi', function (req, res) {
+    var timeout = req.param('timeout', 0);
+    provider.findAll(function (err, data) {
+        if (err) throw err;
+        setTimeout(function () {
             res.json(data);
-        }, 1000);
+        }, timeout);
     });
 });
 
-app.get('/api/eventi/:id', function(req, res){
-    provider.findById(req.params.id, function(err, album){
-        if(err) throw err;
-        res.json(album);
+app.get('/api/eventi/:id', function (req, res) {
+    provider.findById(req.params.id, function (err, evento) {
+        if (err) throw err;
+        res.json(evento);
     });
 });
 
-app.get('/jqm', function(req, res){
+app.get('/jqm', function (req, res) {
     res.redirect('/jquerymobile/demos/index.html');
 });
 
-app.post('/api/album/', function(req,res){
-    console.log('start');
+app.post('/api/eventi/', function (req, res) {
     console.log(req.body);
-    console.log('stop');
-    res.json({status: req.body});
+    provider.save({title:req.body.title, location:req.body.location}, function (err, eventi) {
+        if (err) throw err;
+        res.json(eventi[eventi.length-1]);
+    });
 });
 
 app.listen(3000);
